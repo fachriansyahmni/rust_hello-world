d6b46600da2f985048a93c48024b27a9afc685a1
fachriansyahmni <fachriansyahmni@gmail.com>
2021-10-14 20:19:55 +0700

    hello world

---
+++ doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"helloworld":{"doc":"","t":[5],"n":["hello"],"q":["helloworld"],"d":[""],"i":[0],"f":[[[]]],"p":[]},\
+"lib":{"doc":"","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ doc/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["helloworld"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["lib"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
